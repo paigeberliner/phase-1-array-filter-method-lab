733bcbd1aec02bb189119701dabc1751fcbb1f98
Paige <paigeberliner@gmail.com>
2023-08-14 18:45:33 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution here
+
+function findMatching(drivers, string) {
+    const matchingDrivers = [];
+    for (const driver of drivers) {
+      if (driver.toLowerCase() === string.toLowerCase()) {
+        matchingDrivers.push(driver);
+      }
+    }
+    return matchingDrivers;
+  }
+
+  function fuzzyMatch(drivers, string) {
+    const firstLetter = [];
+    for (const driver of drivers) {
+      if (driver.startsWith(string)) {
+        firstLetter.push(driver);
+      }
+    }
+  
+    return firstLetter;
+  }
+
+  function matchName(drivers, string, name) {
+    const matchingObject = [];
+    for (const driver of drivers) {
+      if (driver.name === string) {
+        matchingObject.push(driver);
+      }
+    }
+    return matchingObject;
+  }
+  
+
+
+
+
